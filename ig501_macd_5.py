4afd7dbf63e5aa948f9963947df05b0608b5b3d1
BZ <435878393@qq.com>
2020-12-07 23:59:40 +0800

    first

---
+++ ig501_macd_5.py
@@ -0,0 +1,87 @@
+import json
+import logging
+import sched
+import time
+from datetime import datetime
+
+import numpy
+import pandas
+import requests
+import talib
+
+from ftqq_tokens import ftqq_tokens
+
+schdule = sched.scheduler(time.time, time.sleep)
+sign_len = 0
+old_date = ""
+
+
+def func():
+    global sign_len
+    global old_date
+    res = requests.post(
+        'http://ig507.com/data/time/history/trade/600196/5?licence=985508AC-EE35-5AC5-2B90-0A74F5AA20FB')
+    try:
+        data_json_list = json.loads(res.text)
+        data_df = pandas.DataFrame(data_json_list)
+        data_df['macd'], data_df['signal'], data_df['hist'] = talib.MACD(data_df['c'])
+
+        date_str = datetime.now().strftime("%Y-%m-%d")
+        # date_str = '2020-12-03'
+        if date_str != old_date:
+            old_date = date_str
+            sign_len = 0
+        data_df_today = data_df[data_df['d'].str.contains(date_str)]
+        data_df_today['date'] = data_df_today['d'].str[11:]
+        print(data_df_today[['date', 'c', 'macd', 'signal', 'hist']].to_markdown(index=False))
+
+        is_list = ['']
+        for i in range(1, data_df_today.shape[0]):
+            before = data_df_today.iloc[i - 1]['hist']
+            now = data_df_today.iloc[i]['hist']
+            if before <= 0 and now > 0:
+                is_list.append("macd由负变正发生反转")
+            elif before >= 0 and now < 0:
+                is_list.append("macd由正变负发生反转")
+            elif numpy.sign(now + now - before) != numpy.sign(now):
+                if now > 0:
+                    is_list.append("macd即将由正变负发生反转")
+                else:
+                    is_list.append("macd即将由负变正发生反转")
+            else:
+                is_list.append('')
+        # data_df_today['is'] = is_list
+        data_df_today.loc[:, 'is'] = is_list
+        # print(data_df_today)
+
+        data_df_is = data_df_today[data_df_today['is'] != '']
+        if not data_df_is.empty and data_df_is.shape[0] > sign_len:
+            sign_len = data_df_is.shape[0]
+            print(data_df_is)
+
+            # data_df_result = data_df_is[['date', 'c', 'macd', 'signal', 'hist', 'is']]
+            data_df_result = data_df_is[['date', 'c', 'is']]
+            data_df_result[' '] = '&nbsp;&nbsp;&nbsp;&nbsp;'
+            data_df_result = data_df_result[['date', ' ', 'c', ' ', 'is']]
+            data_df_result = data_df_result.rename(columns={"date": "time", "c": "股价", "is": "指标"})
+            try:
+                result_markdown = data_df_result.to_markdown(index=False)
+            except Exception as e:
+                result_markdown = data_df_result.to_markdown(showindex=False)
+            print(result_markdown)
+
+            for ftqq_token in ftqq_tokens:
+                res = requests.post('https://sc.ftqq.com/{}.send'.format(ftqq_token),
+                                    data={'text': 'ig507_600196',
+                                          'desp': result_markdown + "\n\n" + datetime.now().strftime(
+                                              "%Y-%m-%d %H:%M:%S")})
+            print(res.text)
+    except Exception as e:
+        logging.warning("handle data error.", e)
+
+    print(datetime.now())
+    schdule.enter(60 * 5, 0, func)
+
+
+schdule.enter(0, 0, func)
+schdule.run()
