d9cbec8457ca4ba3e09ba1f8787fc4fd1b1fcf12
BZ <435878393@qq.com>
2020-12-15 02:15:15 +0800

    保存tushare

--- DATABASE/__init__.py
+++ DATABASE/__init__.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 def database_factory(database_name, sheet_name, model="pymongo"):
     if model == "pymongo":
         from .database_pymongo import DataBasePyMongo
-        return DataBasePyMongo(database_name=database_name, sheet_name=sheet_name, host='172.17.0.5', port=49154)
+        return DataBasePyMongo(database_name=database_name, sheet_name=sheet_name, host='192.168.10.5', port=49154)
     else:
         raise ValueError('model name: {} can not find.'.format(model))
         pass
