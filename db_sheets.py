27415c8e10f84393b877373b9cae41cc2ada1a2f
BZ <435878393@qq.com>
2020-12-29 22:38:23 +0800

    新增股票600559老白干

--- db_sheets.py
+++ db_sheets.py
@@ -6,6 +6,7 @@ db_sheets = {
     "600196": database_factory(database_name="tushare", sheet_name="sh_600196", model="pymongo"),
     "600511": database_factory(database_name="tushare", sheet_name="sh_600511", model="pymongo"),
     "601012": database_factory(database_name="tushare", sheet_name="sh_601012", model="pymongo"),
+    "600559": database_factory(database_name="tushare", sheet_name="sh_600559", model="pymongo"),
 }
 
 db_redis = redis.Redis(host='192.168.10.5', port=6379, db=0)
